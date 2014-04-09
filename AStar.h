6ab56cfbc7aa76f99f78f2de32edd763246b3932
Michael Klostermann <klostermann@mvtec.com>
2014-04-09 13:24:14 +0200

    Copyright update

--- AStar.h
+++ AStar.h
@@ -1,6 +1,7 @@
 /*
  Copyright (c) 2012, Sean Heber. All rights reserved.
- 
+ Copyright (c) 2014, Michael Klostermann. Not all rights reserved.
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
  
